dc9c9cb2698e3ab1d1774143b05d48eccde73dfb
viceamarian <145567138+viceamarian@users.noreply.github.com>
2023-11-09 16:27:39 +0200

    feat(ui_localizations): Add romanian localization (#174)

---
+++ packages/firebase_ui_localizations/lib/src/lang/ro.dart
@@ -0,0 +1,331 @@
+// Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import '../default_localizations.dart';
+
+class RoLocalizations extends FirebaseUILocalizationLabels {
+  const RoLocalizations();
+
+  @override
+  String get accessDisabledErrorText =>
+      "Accesul la acest cont a fost dezactivat temporar";
+
+  @override
+  String get arrayLabel => "matrice";
+
+  @override
+  String get booleanLabel => "boolean";
+
+  @override
+  String get cancelLabel => "anulează";
+
+  @override
+  String get chooseACountry => "Alege o țară";
+
+  @override
+  String get confirmPasswordDoesNotMatchErrorText => "Parolele nu se potrivesc";
+
+  @override
+  String get confirmPasswordInputLabel => "Confirmă parola";
+
+  @override
+  String get confirmPasswordIsRequiredErrorText => "Confirmă parola";
+
+  @override
+  String get continueText => "Continuă";
+
+  @override
+  String get countryCode => "Cod";
+
+  @override
+  String get credentialAlreadyInUseErrorText =>
+      "Acest furnizor este asociat cu un alt cont de utilizator.";
+
+  @override
+  String get deleteAccount => "Șterge cont";
+
+  @override
+  String get differentMethodsSignInTitleText =>
+      "Utilizează una dintre următoarele metode pentru a te conecta";
+
+  @override
+  String get disable => "Dezactivează";
+
+  @override
+  String get eastInitialLabel => "E";
+
+  @override
+  String get emailInputLabel => "Email";
+
+  @override
+  String get emailIsRequiredErrorText => "Emailul este obligatoriu";
+
+  @override
+  String get emailLinkSignInButtonLabel => "Conectați-vă cu linkul magic";
+
+  @override
+  String get emailTakenErrorText => "Există deja un cont cu acest email";
+
+  @override
+  String get enable => "Activează";
+
+  @override
+  String get enableMoreSignInMethods =>
+      "Activează mai multe metode de conectare";
+
+  @override
+  String get enterSMSCodeText => "Introdu codul SMS";
+
+  @override
+  String get findProviderForEmailTitleText =>
+      "Introdu adresa de email pentru a continua";
+
+  @override
+  String get forgotPasswordButtonLabel => "Parolă uitată?";
+
+  @override
+  String get forgotPasswordHintText =>
+      "Furnizează adresa de email și îți vom trimite un link pentru a îți reseta parola";
+
+  @override
+  String get forgotPasswordViewTitle => "Parolă uitată";
+
+  @override
+  String get geopointLabel => "geopunct";
+
+  @override
+  String get goBackButtonLabel => "Înapoi";
+
+  @override
+  String get invalidCountryCode => "Cod invalid";
+
+  @override
+  String get isNotAValidEmailErrorText => "Furnizează un email valid";
+
+  @override
+  String get latitudeLabel => "latitudine";
+
+  @override
+  String get linkEmailButtonText => "Următorul";
+
+  @override
+  String get longitudeLabel => "longitudine";
+
+  @override
+  String get mapLabel => "hartă";
+
+  @override
+  String get mfaTitle => "Verificare în doi pași";
+
+  @override
+  String get name => "Nume";
+
+  @override
+  String get northInitialLabel => "N";
+
+  @override
+  String get nullLabel => "null";
+
+  @override
+  String get numberLabel => "număr";
+
+  @override
+  String get off => "Oprit";
+
+  @override
+  String get on => "Pornit";
+
+  @override
+  String get passwordInputLabel => "Parolă";
+
+  @override
+  String get passwordIsRequiredErrorText => "Parola este obligatorie";
+
+  @override
+  String get passwordResetEmailSentText =>
+      "Ți-am trimis un email cu un link pentru a îți reseta parola. Te rugăm să îți verifici emailurile.";
+
+  @override
+  String get phoneInputLabel => "Număr de telefon";
+
+  @override
+  String get phoneNumberInvalidErrorText => "Numărul de telefon nu e valid";
+
+  @override
+  String get phoneNumberIsRequiredErrorText =>
+      "Numărul de telefon nu este obligatoriu";
+
+  @override
+  String get phoneVerificationViewTitleText => "Întrodu numărul de telefon";
+
+  @override
+  String get profile => "Profil";
+
+  @override
+  String get provideEmail => "Furnizează adresa de email și parola";
+
+  @override
+  String get referenceLabel => "referință";
+
+  @override
+  String get registerActionText => "Înregistrează-te";
+
+  @override
+  String get registerHintText => "Nu ai un cont?";
+
+  @override
+  String get registerText => "Înregistrare";
+
+  @override
+  String get resetPasswordButtonLabel => "Resetează parola";
+
+  @override
+  String get sendLinkButtonLabel => "Trimite linkul magic";
+
+  @override
+  String get signInActionText => "Conectare";
+
+  @override
+  String get signInHintText => "Ai deja un cont?";
+
+  @override
+  String get signInMethods => "Moduri de conectare";
+
+  @override
+  String get signInText => "Conectare";
+
+  @override
+  String get signInWithAppleButtonText => "Conectare cu Apple";
+
+  @override
+  String get signInWithEmailLinkSentText =>
+      "Ți-am trimis un email cu un link magic. Verifică emailul și urmează linkul pentru a te conecta.";
+
+  @override
+  String get signInWithEmailLinkViewTitleText => "Conectare cu link magic";
+
+  @override
+  String get signInWithFacebookButtonText => "Conectare cu Facebook";
+
+  @override
+  String get signInWithGoogleButtonText => "Conectare cu Google";
+
+  @override
+  String get signInWithPhoneButtonText => "Conectare cu telefon";
+
+  @override
+  String get signInWithTwitterButtonText => "Conectare cu Twitter";
+
+  @override
+  String get signOutButtonText => "Deconectare";
+
+  @override
+  String get smsAutoresolutionFailedError =>
+      "Nu s-a rezolvat automat codul SMS. Te rugăm să introduci codul manual";
+
+  @override
+  String get southInitialLabel => "S";
+
+  @override
+  String get stringLabel => "șir";
+
+  @override
+  String get timestampLabel => "timestamp";
+
+  @override
+  String get typeLabel => "tip";
+
+  @override
+  String get unknownError => "O eroare necunoscută s-a produs";
+
+  @override
+  String get updateLabel => "actualizează";
+
+  @override
+  String get userNotFoundErrorText => "Contul nu există";
+
+  @override
+  String get valueLabel => "valoare";
+
+  @override
+  String get verifyCodeButtonText => "Verifică";
+
+  @override
+  String get verifyingSMSCodeText => "Se verifică codul SMS...";
+
+  @override
+  String get verifyItsYouText => "Verifică dacă ești tu";
+
+  @override
+  String get verifyPhoneNumberButtonText => "Următorul";
+
+  @override
+  String get westInitialLabel => "W";
+
+  @override
+  String get wrongOrNoPasswordErrorText =>
+      "Parola nu este validă sau utilizatorul nu are o parolă";
+
+  @override
+  String get uploadButtonText => "Încarcă fișier";
+
+  @override
+  String get verifyEmailTitle => "Verifică emailul";
+
+  @override
+  String get verificationEmailSentText =>
+      "Un email de verificare a fost trimis la adresa ta de email. Te rugăm să îți verifici emailul și să faci clic pe link pentru a îți confirma adresa de email.";
+
+  @override
+  String get verificationFailedText =>
+      "Nu am putut verifica adresa ta de email.";
+
+  @override
+  String get resendVerificationEmailButtonLabel =>
+      "Retrimite emailul de verificare";
+
+  @override
+  String get verificationEmailSentTextShort =>
+      "Emailul de verificare a fost trimis";
+
+  @override
+  String get emailIsNotVerifiedText => "Emailul nu este verificat";
+
+  @override
+  String get waitingForEmailVerificationText =>
+      "Se așteaptă verificarea emailului";
+
+  @override
+  String get dismissButtonLabel => "Respingeți";
+
+  @override
+  String get okButtonLabel => "OK";
+
+  @override
+  String get checkEmailHintText =>
+      "Te rugăm să îți verifici emailul și să faci clic pe link pentru a îți confirma adresa de e-mail.";
+
+  @override
+  String get doneButtonLabel => "Gata";
+
+  @override
+  String get invalidVerificationCodeErrorText =>
+      "Codul introdus nu este valid. Te rugăm să încerci din nou.";
+
+  @override
+  String get ulinkProviderAlertTitle => "Deconectați furnizorul";
+
+  @override
+  String get confirmUnlinkButtonLabel => "Deconectați";
+
+  @override
+  String get cancelButtonLabel => "Anulează";
+
+  @override
+  String get unlinkProviderAlertMessage =>
+      "Sigur dorești să deconectezi acest furnizor?";
+
+  @override
+  String get weakPasswordErrorText =>
+      "Parola trebuie să aibă cel puțin 6 caractere";
+}
