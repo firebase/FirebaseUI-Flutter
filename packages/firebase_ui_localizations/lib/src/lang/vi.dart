689af2738a5afb11704874c20a5d37c5c314c677
Trung <handsomezezo@gmail.com>
2023-12-23 23:30:20 +0700

    add vietnamese

---
+++ packages/firebase_ui_localizations/lib/src/lang/vi.dart
@@ -0,0 +1,336 @@
+// Copyright 2023, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import '../default_localizations.dart';
+
+class ViLocalizations extends FirebaseUILocalizationLabels {
+  const ViLocalizations();
+
+  @override
+  String get accessDisabledErrorText =>
+      "Tài khoản này đã bị tạm thời vô hiệu hóa";
+
+  @override
+  String get arrayLabel => "mảng";
+
+  @override
+  String get booleanLabel => "Boolean";
+
+  @override
+  String get cancelLabel => "hủy";
+
+  @override
+  String get chooseACountry => "Chọn một quốc gia";
+
+  @override
+  String get confirmPasswordDoesNotMatchErrorText => "Mật khẩu không khớp";
+
+  @override
+  String get confirmPasswordInputLabel => "Xác nhận mật khẩu";
+
+  @override
+  String get confirmPasswordIsRequiredErrorText =>
+      "Vui lòng xác nhận mật khẩu của bạn";
+
+  @override
+  String get continueText => "Tiếp tục";
+
+  @override
+  String get countryCode => "Mã";
+
+  @override
+  String get credentialAlreadyInUseErrorText =>
+      "Nhà cung cấp này đã được liên kết với một tài khoản người dùng khác.";
+
+  @override
+  String get deleteAccount => "Xóa tài khoản";
+
+  @override
+  String get differentMethodsSignInTitleText =>
+      "Sử dụng một trong các phương thức sau để đăng nhập";
+
+  @override
+  String get disable => "Tắt";
+
+  @override
+  String get eastInitialLabel => "Đ";
+
+  @override
+  String get emailInputLabel => "Email";
+
+  @override
+  String get emailIsRequiredErrorText => "Email là bắt buộc";
+
+  @override
+  String get emailLinkSignInButtonLabel => "Đăng nhập bằng liên kết phép màu";
+
+  @override
+  String get emailTakenErrorText => "Một tài khoản với email này đã tồn tại";
+
+  @override
+  String get enable => "Bật";
+
+  @override
+  String get enableMoreSignInMethods => "Bật thêm phương thức đăng nhập";
+
+  @override
+  String get enterSMSCodeText => "Nhập mã SMS";
+
+  @override
+  String get findProviderForEmailTitleText => "Nhập email để tiếp tục";
+
+  @override
+  String get forgotPasswordButtonLabel => "Quên mật khẩu?";
+
+  @override
+  String get forgotPasswordHintText =>
+      "Cung cấp email của bạn và chúng tôi sẽ gửi cho bạn một liên kết để đặt lại mật khẩu của bạn";
+
+  @override
+  String get forgotPasswordViewTitle => "Quên mật khẩu";
+
+  @override
+  String get geopointLabel => "điểm địa lý";
+
+  @override
+  String get goBackButtonLabel => "Quay lại";
+
+  @override
+  String get invalidCountryCode => "Mã không hợp lệ";
+
+  @override
+  String get isNotAValidEmailErrorText => "Hãy cung cấp một email hợp lệ";
+
+  @override
+  String get latitudeLabel => "vĩ độ";
+
+  @override
+  String get linkEmailButtonText => "Tiếp theo";
+
+  @override
+  String get longitudeLabel => "kinh độ";
+
+  @override
+  String get mapLabel => "bản đồ";
+
+  @override
+  String get mfaTitle => "Xác thực 2 bước";
+
+  @override
+  String get name => "Tên";
+
+  @override
+  String get northInitialLabel => "B";
+
+  @override
+  String get nullLabel => "null";
+
+  @override
+  String get numberLabel => "số";
+
+  @override
+  String get off => "Tắt";
+
+  @override
+  String get on => "Bật";
+
+  @override
+  String get passwordInputLabel => "Mật khẩu";
+
+  @override
+  String get passwordIsRequiredErrorText => "Mật khẩu là bắt buộc";
+
+  @override
+  String get passwordResetEmailSentText =>
+      "Chúng tôi đã gửi cho bạn một email với liên kết để đặt lại mật khẩu của bạn. Vui lòng kiểm tra email của bạn.";
+
+  @override
+  String get phoneInputLabel => "Số điện thoại";
+
+  @override
+  String get phoneNumberInvalidErrorText => "Số điện thoại không hợp lệ";
+
+  @override
+  String get phoneNumberIsRequiredErrorText => "Số điện thoại là bắt buộc";
+
+  @override
+  String get phoneVerificationViewTitleText => "Nhập số điện thoại của bạn";
+
+  @override
+  String get profile => "Hồ sơ";
+
+  @override
+  String get provideEmail => "Cung cấp email và mật khẩu của bạn";
+
+  @override
+  String get referenceLabel => "tham chiếu";
+
+  @override
+  String get registerActionText => "Đăng ký";
+
+  @override
+  String get registerHintText => "Chưa có tài khoản?";
+
+  @override
+  String get registerText => "Đăng ký";
+
+  @override
+  String get resetPasswordButtonLabel => "Đặt lại mật khẩu";
+
+  @override
+  String get sendLinkButtonLabel => "Gửi liên kết ma thuật";
+
+  @override
+  String get signInActionText => "Đăng nhập";
+
+  @override
+  String get signInHintText => "Đã có tài khoản?";
+
+  @override
+  String get signInMethods => "Phương thức đăng nhập";
+
+  @override
+  String get signInText => "Đăng nhập";
+
+  @override
+  String get signInWithAppleButtonText => "Đăng nhập bằng Apple";
+
+  @override
+  String get signInWithEmailLinkSentText =>
+      "Chúng tôi đã gửi cho bạn một email với một liên kết ma thuật. Kiểm tra email của bạn và theo liên kết để đăng nhập.";
+
+  @override
+  String get signInWithEmailLinkViewTitleText =>
+      "Đăng nhập bằng liên kết ma thuật";
+
+  @override
+  String get signInWithFacebookButtonText => "Đăng nhập bằng Facebook";
+
+  @override
+  String get signInWithGoogleButtonText => "Đăng nhập bằng Google";
+
+  @override
+  String get signInWithPhoneButtonText => "Đăng nhập bằng điện thoại";
+
+  @override
+  String get signInWithTwitterButtonText => "Đăng nhập bằng Twitter";
+
+  @override
+  String get signOutButtonText => "Đăng xuất";
+
+  @override
+  String get smsAutoresolutionFailedError =>
+      "Không thể giải quyết mã xác minh SMS tự động. Vui lòng nhập mã của bạn thủ công.";
+
+  @override
+  String get southInitialLabel => "S";
+
+  @override
+  String get stringLabel => "chuỗi";
+
+  @override
+  String get timestampLabel => "mốc thời gian";
+
+  @override
+  String get typeLabel => "kiểu";
+
+  @override
+  String get unknownError => "Đã xảy ra một lỗi không xác định";
+
+  @override
+  String get updateLabel => "cập nhật";
+
+  @override
+  String get userNotFoundErrorText => "Tài khoản không tồn tại";
+
+  @override
+  String get valueLabel => "giá trị";
+
+  @override
+  String get verifyCodeButtonText => "Xác minh";
+
+  @override
+  String get verifyingSMSCodeText => "Đang xác minh mã SMS...";
+
+  @override
+  String get verifyItsYouText => "Xác minh rằng đó là bạn";
+
+  @override
+  String get verifyPhoneNumberButtonText => "Tiếp theo";
+
+  @override
+  String get westInitialLabel => "W";
+
+  @override
+  String get wrongOrNoPasswordErrorText =>
+      "Mật khẩu không hợp lệ hoặc người dùng không có mật khẩu";
+
+  @override
+  String get uploadButtonText => "Tải lên tệp";
+
+  @override
+  String get verifyEmailTitle => "Xác minh địa chỉ email của bạn";
+
+  @override
+  String get verificationEmailSentText =>
+      "Một email xác minh đã được gửi đến địa chỉ email của bạn. Vui lòng kiểm tra email của bạn và nhấp vào liên kết để xác minh địa chỉ email của bạn.";
+
+  @override
+  String get verificationFailedText =>
+      "Chúng tôi không thể xác minh địa chỉ email của bạn. ";
+
+  @override
+  String get resendVerificationEmailButtonLabel => "Gửi lại email xác minh";
+
+  @override
+  String get verificationEmailSentTextShort => "Email xác minh đã được gửi đi";
+
+  @override
+  String get emailIsNotVerifiedText => "Email chưa được xác minh";
+
+  @override
+  String get waitingForEmailVerificationText => "Đang chờ xác minh email";
+
+  @override
+  String get dismissButtonLabel => "Bỏ qua";
+
+  @override
+  String get okButtonLabel => "OK";
+
+  @override
+  String get checkEmailHintText =>
+      "Vui lòng kiểm tra email của bạn và nhấp vào liên kết để xác minh địa chỉ email của bạn.";
+
+  @override
+  String get doneButtonLabel => "Hoàn tất";
+
+  @override
+  String get invalidVerificationCodeErrorText =>
+      "Mã bạn nhập không hợp lệ. Vui lòng thử lại.";
+
+  @override
+  String get unlinkProviderAlertTitle => "Hủy liên kết nhà cung cấp";
+
+  @override
+  String get confirmUnlinkButtonLabel => "Hủy liên kết";
+
+  @override
+  String get cancelButtonLabel => "Hủy";
+
+  @override
+  String get unlinkProviderAlertMessage =>
+      "Bạn có chắc chắn muốn hủy liên kết với nhà cung cấp này không?";
+
+  @override
+  String get weakPasswordErrorText => "Mật khẩu phải có ít nhất 6 ký tự";
+
+  @override
+  String get confirmDeleteAccountAlertTitle => "Xác nhận xóa tài khoản";
+
+  @override
+  String get confirmDeleteAccountAlertMessage =>
+      "Bạn có chắc chắn muốn xóa tài khoản của bạn không?";
+
+  @override
+  String get confirmDeleteAccountButtonLabel => "Có, xóa";
+}
