955e5c286ea5835defe0bd922428102e98f9e8d7
kzrnm <gengesa@gmail.com>
2024-04-16 21:10:31 +0900

    fix(firebase_ui_auth): `sendPasswordResetEmail` should not be executed when `isLoading=true` (#315)
    Co-authored-by: russellwheatley <russellwheatley85@gmail.com>

--- packages/firebase_ui_auth/test/views/forgot_password_view_test.dart
+++ packages/firebase_ui_auth/test/views/forgot_password_view_test.dart
@@ -4,6 +4,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart' as fba;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_shared/firebase_ui_shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -53,6 +54,10 @@ void main() {
       when(auth.sendPasswordResetEmail(email: 'valid@email.com')).thenAnswer(
         (_) => Future.value(),
       );
+
+      when(auth.sendPasswordResetEmail(email: 'delay@email.com')).thenAnswer(
+        (_) => Future.delayed(const Duration(milliseconds: 200)),
+      );
     });
 
     testWidgets('shows error if sendPasswordResetEmail failed', (tester) async {
@@ -81,5 +86,20 @@ void main() {
         expect(find.byType(ErrorText), findsNothing);
       },
     );
+
+    testWidgets("doesn't sendPasswordResetEmail on loading", (tester) async {
+      await tester.pumpWidget(widget);
+
+      final input = find.byType(TextField);
+      await tester.enterText(input, 'delay@email.com');
+
+      final button = find.byType(LoadingButton);
+      await tester.tap(button);
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ErrorText), findsNothing);
+      verify(auth.sendPasswordResetEmail(email: 'delay@email.com')).called(1);
+    });
   });
 }
