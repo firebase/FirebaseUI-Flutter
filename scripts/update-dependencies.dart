32e38188a777850c819798c10bdc8bc74e034f79
russellwheatley <russellwheatley85@gmail.com>
2024-07-02 16:00:45 +0100

    ci: update dependencies with script

---
+++ scripts/update-dependencies.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path/path.dart' as p;
+import 'package:yaml/yaml.dart';
+import 'package:yaml_edit/yaml_edit.dart';
+
+void main(List<String> args) async {
+  const bomPath =
+      'https://raw.githubusercontent.com/firebase/flutterfire/master/scripts/versions.json';
+  final http = HttpClient();
+  final request = await http.getUrl(Uri.parse(bomPath));
+  final response = await request.close(); // sends the request
+  final jsonString = await response.transform(utf8.decoder).join();
+  final json = jsonDecode(jsonString) as Map<String, dynamic>;
+
+  final latestVersions =
+      json[json.keys.first]["packages"] as Map<String, dynamic>;
+
+  final iosSdkVersion = json[json.keys.first]["firebase_sdk"]["ios"] as String;
+
+  final pubspecFilePaths = findPubspecFiles(Directory.current);
+
+  final listOfFuture = <Future>[];
+  for (final filePath in pubspecFilePaths) {
+    final future = updatePubspecFile(filePath, latestVersions);
+    listOfFuture.add(future);
+  }
+
+  listOfFuture.add(
+    updatePodfileVersion(
+      iosSdkVersion,
+      './tests/ios/Podfile',
+    ),
+  );
+  listOfFuture.add(
+    updatePodfileVersion(
+      iosSdkVersion,
+      './tests/macos/Podfile',
+    ),
+  );
+
+  await Future.wait(listOfFuture);
+}
+
+Future<void> updatePubspecFile(
+    String filePath, Map<String, dynamic> latestVersions) async {
+  final content = await File(filePath).readAsString();
+  final yamlEditor = YamlEditor(content);
+  final pubspec = loadYaml(content) as YamlMap;
+
+  final dependencies = pubspec['dependencies'] as YamlMap?;
+
+  if (dependencies != null) {
+    dependencies.forEach((key, value) {
+      if (latestVersions.containsKey(key)) {
+        yamlEditor.update(['dependencies', key], '^${latestVersions[key]}');
+      }
+    });
+  }
+
+  await File(filePath).writeAsString(yamlEditor.toString());
+}
+
+List<String> findPubspecFiles(Directory root) {
+  final pubspecFiles = <String>[];
+  final directories = [
+    Directory(p.join(root.path, 'packages')),
+    Directory(p.join(root.path, 'tests'))
+  ];
+
+  for (final dir in directories) {
+    if (dir.existsSync()) {
+      dir.listSync(recursive: true).forEach((entity) {
+        if (entity is File && p.basename(entity.path) == 'pubspec.yaml') {
+          if (!entity.path.contains('.symlinks') &&
+              !entity.path.contains('.plugin_symlinks')) {
+            pubspecFiles.add(entity.path);
+          }
+        }
+      });
+    }
+  }
+
+  return pubspecFiles;
+}
+
+Future<void> updatePodfileVersion(
+    String iosSdkVersion, String podfilePath) async {
+  final podfile = File(podfilePath);
+  if (!await podfile.exists()) {
+    throw Exception('Podfile not found at $podfilePath');
+  }
+
+  String content = await podfile.readAsString();
+
+  // Update the version
+  final updatedContent = content.replaceAllMapped(
+    RegExp(
+        r"pod 'FirebaseFirestore', :git => 'https://github.com/invertase/firestore-ios-sdk-frameworks.git', :tag => '\d+\.\d+\.\d+'"),
+    (match) =>
+        "pod 'FirebaseFirestore', :git => 'https://github.com/invertase/firestore-ios-sdk-frameworks.git', :tag => '$iosSdkVersion'",
+  );
+
+  // Write the updated content back to the Podfile
+  await podfile.writeAsString(updatedContent);
+
+  print('Updated Podfile version to $iosSdkVersion');
+}
