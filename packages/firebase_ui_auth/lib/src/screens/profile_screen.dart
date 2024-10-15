a3fe57cdb4698f4a57ae0a7927ff5c0b53321c10
Dario Digregorio <32966133+dario-digregorio@users.noreply.github.com>
2024-10-15 10:18:02 +0200

    using theme color instead of hardcoded value

--- packages/firebase_ui_auth/lib/src/screens/profile_screen.dart
+++ packages/firebase_ui_auth/lib/src/screens/profile_screen.dart
@@ -387,7 +387,7 @@ class _EmailVerificationBadgeState extends State<_EmailVerificationBadge> {
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.yellow,
+              color: Theme.of(context).colorScheme.surface,
               borderRadius: BorderRadius.circular(12),
             ),
             child: Padding(
