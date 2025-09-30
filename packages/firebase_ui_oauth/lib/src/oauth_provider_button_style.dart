b0f9bc32e35f309b5f52a49989eda20b900b8ac1
russellwheatley <russellwheatley85@gmail.com>
2025-09-30 11:27:50 +0100

    fix: analyser issues from later Flutter version

--- packages/firebase_ui_oauth/lib/src/oauth_provider_button_style.dart
+++ packages/firebase_ui_oauth/lib/src/oauth_provider_button_style.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:firebase_ui_shared/firebase_ui_shared.dart';
